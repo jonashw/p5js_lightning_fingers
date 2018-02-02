d05b539cc9a50bdb25f9dc7b50ef4031e2acaf5f
jonashw <2jonwilson@gmail.com>
2018-02-02 02:34:12 -0800

    added lightning effect between center and each touch; uses Perlin noise for the pseudo-random walk

--- src/Background.js
+++ src/Background.js
@@ -6,6 +6,12 @@ function Background(colors){
   };
 
   this.draw = function(){
-    background(_colors.getCurrent());
+    let color = _colors.getCurrent();
+    background(color);
+    /* The following does not work as expected.  
+    ** I need to learn more about p5.Color! */
+    //let p = 50;
+    //let l = color.levels;
+    //text(`HSL(${l[0]},${l[1]},${l[2]})`, p, height - p, width, height);
   };
 }
--- src/sketch.js
+++ src/sketch.js
@@ -1,10 +1,18 @@
 /*jshint esversion: 6 */
 var bg;
 var playing = false;
+var updating = true;
+var drawing = true;
 var touchColor;
+var center = {x:0, y:0};
+let lightningColors = [];
 function setup() {
   colorMode(HSL, 255, 255, 255, 100);
-  touchColor = color(0,0,0,50);
+  //greatly improve performance with a pixelDensity less than 1:
+  //pixelDensity(0.125);
+  pixelDensity(1);
+  //alert('pixelDensity: ' + pixelDensity());
+  //alert('displayDensity: ' + displayDensity());
   bg = new Background((() => {
     var _hues = [];
     let bgLerpFactor = 8;
@@ -15,35 +23,98 @@ function setup() {
   })());
   angleMode(DEGREES);
   createCanvas(windowWidth, windowHeight);
+  windowResized();
+  lightningColors = countFrom(200,55).map(l => color(l,255,100));
 }
 
-function draw() {
-  update();
-  bg.draw();
-  touches.forEach(t => {
-    noStroke();
-    fill(touchColor);
-    ellipse(t.x, t.y, 200, 200);
-  });
-}
-
-function update(){
-  bg.update();
-  if(!playing){
-    return;
+function range(min, max){
+  let values = [];
+  for(let n=min; n<=max; n++){
+    values.push(n);
   }
+  return values;
 }
 
-function touchStarted(){
-  var lastTouch = touches.slice().pop();
-  return false; // This is to prevent pinch-zooming on touch devices.
+function countFrom(from, count){
+  let values = [];
+  for(let i=0; i<count; i++){
+    values.push(from + i);
+  }
+  return values;
 }
-
-function mousePressed(){
-  return false;
-}
-
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
+  center.x = windowWidth/2;
+  center.y = windowHeight/2;
+}
+/* This is to prevent pinch-zooming on touch devices: */
+function touchStarted(){ return false; }
+/* I'm not sure if this is strictly necessary: */
+function mousePressed(){ return false; }
+
+function lightning(x1,y1,x2/*optional*/,y2/*optional*/){
+  let x = x1; 
+  let y = y1;
+  let x_next, y_next;
+  let d = 50 * displayDensity();
+  let n = 10;
+  let color = lightningColors[parseInt(noise(frameCount/30) * lightningColors.length) - 1];
+  if(color){
+    stroke(color);
+    fill(color);
+  }
+  let C = 2 * d / n;
+  let HC = C / 2;
+  strokeWeight(5 * displayDensity());
+  if(x2 != undefined && y2 != undefined){
+    for(var i=n; i>0; i--){
+      dx = (x - x2) / i;
+      dy = (y - y2) / i;
+      x_next = x - dx + (noise(frameCount) * C - HC);
+      y_next = y - dy + (noise(frameCount / 30) * C - HC);
+      line(x, y, x_next, y_next);
+      x = x_next;
+      y = y_next;
+    }
+    line(x, y, x2, y2);
+    noStroke();
+    ellipse(x1, y1, d, d);
+    ellipse(x2, y2, d, d);
+  } else {
+    noStroke();
+    ellipse(x1, y1, d, d);
+  }
+}
+
+function draw() {
+  if(!focused){
+    //return;
+  }
+  update();
+  if(!drawing) return;
+  bg.draw();
+  touches.forEach(t => {
+    lightning(center.x, center.y, t.x, t.y);
+  });
+  if(mouseIsPressed){
+    lightning(center.x, center.y, mouseX, mouseY);
+  }
+  lightning(center.x, center.y);
+}
+
+function update(){
+  if(!updating) return;
+  bg.update();
+}
+
+function keyTyped(){
+  if(key == 'u'){
+    updating = !updating;
+    console.log('updating ' + (updating ? 'resumed' : 'paused'));
+  }
+  if(key == 'd'){
+    drawing = !drawing;
+    console.log('drawing ' + (drawing ? 'resumed' : 'paused'));
+  }
 }
